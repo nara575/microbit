387adc2335cbd3b8f1dc5b86e46a14a2bf8b14d1
nara575 <150215880+nara575@users.noreply.github.com>
2024-03-12 16:11:40 +0900

    Add files via upload

---
+++ 01_내장함수로 하트출력_실습.py
@@ -0,0 +1,14 @@
+'''
+    [내장함수로 하트출력하기]
+
+    1. 모듈 선언하기
+        from microbit import *
+
+        •from: ~로부터
+        •microbit: microbit 모듈
+        •import: 가져오라
+        •*: 모든 명령
+
+    2. 내장된 HEART 이미지 출력
+        display.show(Image.HE)
+'''
